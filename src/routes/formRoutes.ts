552f0f6e47894f9878eafe684259d875e5f89e83
andradew <wilson00749@gmail.com>
2024-07-25 01:14:50 -0500

    add getFormData from apicall or db logic

--- src/routes/formRoutes.ts
+++ src/routes/formRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createFormData } from '../controllers/formController';
+import { createFormData, getFormData } from '../controllers/formController';
 
 const router = express.Router();
 
 // Endpoint para recibir datos del formulario
 router.post('/formdata', createFormData);
+router.get('/formdata', getFormData);
 
 export default router;
